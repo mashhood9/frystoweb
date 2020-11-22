97d274e659358fd6432704e3b724664826c07551
Mashhood Siddiqui <sidmashhood@gmail.com>
2020-11-22 22:40:00 +0530

    Update merchant_signup_credential.js

--- models/merchant_signup_credential.js
+++ models/merchant_signup_credential.js
@@ -7,23 +7,55 @@ const MerchantcredentialsSchema = {
           email: Joi.string().lowercase().regex(regex.email).required().label('Email').error(()=> 'Invalid Email'),
           first_name: Joi.string().trim().regex(regex.only_alphabets).required().label('First Name').error(()=> 'Invalid First Name'),
           last_name: Joi.string().trim().regex(regex.only_alphabets).required().label('Last Name').error(()=> 'Invalid Last Name'),
-          password: Joi.string().required().label('Password').error(()=> 'Invalid Password'),
           mobile_number: Joi.number().required().label('Mobile Number').error(()=> 'Invalid Mobile Number'),
       
         })
     },
     merchant_signin: () => {
       return Joi.object().keys({
-        merchant_frysto_id: Joi.number().required().label('User Role').error(()=> 'Invalid User Role'),
-        password: Joi.string().required().label('Password').error(()=> 'Invalid Password')
+        mobile_number: Joi.number().integer().required(),
       })
     },
-    merchant_data_detail: () => {
+
+    merchant_otpverify: () => {
       return Joi.object().keys({
-        merchant_frysto_id: Joi.number().integer().required(),
+        // email: Joi.string().lowercase().regex(regex.email).required().label('Email').error(()=> 'Invalid Email'),
+        // password: Joi.string().required().label('Password').error(()=> 'Invalid Password')
+        otp_number: Joi.number().required().label('OTP Number').error(()=> 'Invalid OTP Number'),
+        otp_token: Joi.string().trim().required().label('tokenotp').error(()=> 'Invalid TokenOtp'),
+
+      })
+    },
+    merchant_data_detail_validator: () => {
+      return Joi.object().keys({
+        email: Joi.string().lowercase().regex(regex.email).required().label('Email').error(()=> 'Invalid Email'),
         merchant_name: Joi.string().trim().required().label('merchant_name').error(()=> 'Invalid merchant_name'),
         shop_name: Joi.string().trim().required().label('shop_name').error(()=> 'Invalid shop_name'),
         mobile_number: Joi.number().integer().required(),
+        account_number: Joi.number().integer().required(),
+        bank_name: Joi.string().trim().required().label('bank_name').error(()=> 'Invalid bank_name'),
+        ifsc_code: Joi.string().trim().required().label('shop_name').error(()=> 'Invalid ifsc_code'),
+        delivery_range: Joi.number().integer().required(),
+        address: Joi.string().trim().required().label('address').error(()=> 'Invalid address_1'),
+        city: Joi.string().trim().required().label('city').error(()=> 'Invalid city'),
+        shop_latitude: Joi.number().required(),
+        shop_longitude: Joi.number().required(),
+        state: Joi.string().trim().regex(regex.only_alphabets).required().label('state').error(()=> 'Invalid city'),
+        image_url: Joi.string().trim().required().label('image_url').error(()=> 'Invalid image_url'),
+        post_code: Joi.number().required().label('post_code').error(()=> 'Invalid post_code'),
+      })
+    },
+
+    merchant_data_detail: () => {
+      return Joi.object().keys({
+        merchant_frysto_id: Joi.number().integer().required(),
+        email: Joi.string().lowercase().regex(regex.email).required().label('Email').error(()=> 'Invalid Email'),
+        merchant_name: Joi.string().trim().required().label('merchant_name').error(()=> 'Invalid merchant_name'),
+        shop_name: Joi.string().trim().required().label('shop_name').error(()=> 'Invalid shop_name'),
+        mobile_number: Joi.number().integer().required(),
+        account_number: Joi.number().integer().required(),
+        bank_name: Joi.string().trim().required().label('bank_name').error(()=> 'Invalid bank_name'),
+        ifsc_code: Joi.string().trim().required().label('shop_name').error(()=> 'Invalid ifsc_code'),
         delivery_range: Joi.number().integer().required(),
         address: Joi.string().trim().required().label('address').error(()=> 'Invalid address_1'),
         city: Joi.string().trim().required().label('city').error(()=> 'Invalid city'),
