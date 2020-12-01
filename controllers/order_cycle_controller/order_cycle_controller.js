70e34b53cc4c8921db7d91ad4d7b399b73cdb74d
Mashhood Siddiqui <sidmashhood@gmail.com>
2020-12-02 02:38:03 +0530

    Update order_cycle_controller.js

--- controllers/order_cycle_controller/order_cycle_controller.js
+++ controllers/order_cycle_controller/order_cycle_controller.js
@@ -106,7 +106,9 @@ class Order extends BaseModel {
                 productList = await order_list_collection.findOne({ order_id: parseInt(order_id) },
                                                                {
                         projection: {
-                            "product_list": 1
+                            "product_list": 1,
+                            "mode_of_payment":1,
+                            "total_price":1
                         }
                     });
             }
