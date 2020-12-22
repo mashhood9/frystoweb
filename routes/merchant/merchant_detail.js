cfa59fffb2fda866d93004c78bc4c845be51b5f3
Mashhood Siddiqui <sidmashhood@gmail.com>
2020-12-22 14:45:51 +0530

    Update merchant_detail.js

--- routes/merchant/merchant_detail.js
+++ routes/merchant/merchant_detail.js
@@ -66,7 +66,7 @@ router.get("/list/by_lat_lang", async (req, res) => {
         let adminController = new Merchant_Data();
         let MerchantId = req.query.merchant_frysto_id || 0;
         let response = await adminController.getMerchantByMerchantId(MerchantId);
-        res.send({ success : true, data: response, message: 'Use data successfully' });
+        res.send({ success : true, merchant_data: response, message: 'Merchant data successfully' });
     } catch (error) {
         res.status(error.statusCode || 500).send({ success: false, message: error.message });
     }
