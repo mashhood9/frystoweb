69488bbed8a13d7369160bca8564d2be2bbac585
Mashhood Siddiqui <sidmashhood@gmail.com>
2021-01-01 01:18:59 +0530

    change

--- routes/auth/merchant_signup_route.js
+++ routes/auth/merchant_signup_route.js
@@ -8,7 +8,7 @@ router.post('/', async (req, res) => {
     try {
         let signup = new Merchant_Signup();        
         let response = await signup.createUser(req.body);
-        res.status(200).send({ success: true, token: response, message: 'Success', statusCode : 200 });
+        res.send({success:true, merchant_frysto_id:response});
     } catch (error) {
         res.status(error.statusCode || 500).send(error.message);
     }
