513bd1efffd026257bc3f3b36466907e9a5492d4
Mashhood Siddiqui <sidmashhood@gmail.com>
2021-01-31 23:49:40 +0530

    rzp changes

--- controllers/order_cycle_controller/order_cycle_controller.js
+++ controllers/order_cycle_controller/order_cycle_controller.js
@@ -438,7 +438,7 @@ class Order extends BaseModel {
                
                  let pymnt_id=validatedData.payment_id
                  
-                 let url = 'https://rzp_test_iB0O6ZbG60hFox:h6wJkCrlmPXnpYn9H6B28i8S@api.razorpay.com/v1/payments/'+ pymnt_id ;
+                 let url = 'https://${rzp_api}:h6wJkCrlmPXnpYn9H6B28i8S@api.razorpay.com/v1/payments/'+ pymnt_id ;
                  request.get(url, function (error, response, body) {
                  console.log('Response:', body);
                  const obj = JSON.parse(body);
