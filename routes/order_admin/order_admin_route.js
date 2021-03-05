0254981acd956f26f4ec00532c94a75c5c70f763
Mashhood Siddiqui <sidmashhood@gmail.com>
2021-03-05 16:50:11 +0530

    siddiuqi

--- routes/order_admin/order_admin_route.js
+++ routes/order_admin/order_admin_route.js
@@ -3,9 +3,10 @@ var express = require('express');
 var router = express.Router();
 let Order_Admin=require('../../controllers/order_admin/order_controller');
 
-router.get('/orders', async(res) =>{
+router.get('/orders', async(req,res) =>{
     try{
         let order = new Order_Admin();
+        let id = req.query.id;
         let response = await order.getAllOrder();
         res.send({success:true, data:response, message:'orders list'});
     }catch(error){
