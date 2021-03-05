118c37eb1b8de544fd94310a268556db5ec2fe44
Mashhood Siddiqui <sidmashhood@gmail.com>
2021-03-05 16:29:13 +0530

    changes

--- routes/order_admin/order_admin_route.js
+++ routes/order_admin/order_admin_route.js
@@ -7,6 +7,7 @@ router.get('/orders', async(req,res) =>{
     try{
         let order = new Order_Admin();
         let response = await order.getAllOrder();
+        res.send({success:true, data:response, message:'orders list'});
     }catch(error){
         res.status(error.statusCode || 500).send(error.message);
     }
